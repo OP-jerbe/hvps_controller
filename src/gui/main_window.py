2b38d4455cf27eb661bb0ca8f6d5db5eb141c622
fizziksfool <erbe.joshua@gmail.com>
2025-05-29 21:05:44 -0700

    Adds the menu bar and options to the main_window

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,7 +1,22 @@
 from pathlib import Path
 
-from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtCore import QEvent, QObject, QRegularExpression, Qt, Signal
+from PySide6.QtGui import QAction, QIcon, QMouseEvent, QRegularExpressionValidator
+from PySide6.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QFileDialog,
+    QFrame,
+    QGridLayout,
+    QHBoxLayout,
+    QLabel,
+    QLineEdit,
+    QMainWindow,
+    QMessageBox,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
 from qt_material import apply_stylesheet
 
 from helpers.helpers import get_root_dir
@@ -27,3 +42,19 @@ class MainWindow(QMainWindow):
         self.setStyleSheet(
             self.styleSheet() + """QLineEdit, QTextEdit {color: lightgreen;}"""
         )
+
+        # Create the QAction objects for the menus
+        self.exit_option = QAction('Exit')
+        self.exit_option.triggered.connect(QApplication.quit)
+        self.open_user_guide_option = QAction('User Guide')
+        self.open_user_guide_option.triggered.connect(self.open_user_guide)
+
+        # Create the menu bar and menu bar selections
+        self.menu_bar = self.menuBar()
+        self.file_menu = self.menu_bar.addMenu('File')
+        self.file_menu.addAction(self.exit_option)
+        self.help_menu = self.menu_bar.addMenu('Help')
+        self.help_menu.addAction(self.open_user_guide_option)
+
+    def open_user_guide(self) -> None:
+        print('Opened User Guide')
