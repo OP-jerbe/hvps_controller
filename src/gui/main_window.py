8ad5e4b4b713c01225d7b89f8b7a78087dc9345d
OP-jerbe <j.erbe@oregon-physics.com>
2025-05-30 12:41:30 -0700

    Implements the functionality of opening the connection window to connect to the HVPS

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from socket import SocketType
+from typing import Optional
 
 from PySide6.QtCore import QEvent, QObject, QRegularExpression, Qt, Signal
 from PySide6.QtGui import QAction, QIcon, QMouseEvent, QRegularExpressionValidator
@@ -19,13 +21,16 @@ from PySide6.QtWidgets import (
 )
 from qt_material import apply_stylesheet
 
-from helpers.helpers import get_root_dir
+from helpers.helpers import close_socket, get_root_dir
+
+from .open_socket_window import OpenSocketWindow
 
 
 class MainWindow(QMainWindow):
-    def __init__(self, version: str) -> None:
+    def __init__(self, version: str, sock: Optional[SocketType]) -> None:
         super().__init__()
         self.version = version
+        self.sock: Optional[SocketType] = sock
         self.installEventFilter(self)
         self.create_gui()
 
@@ -44,17 +49,39 @@ class MainWindow(QMainWindow):
         )
 
         # Create the QAction objects for the menus
-        self.exit_option = QAction('Exit')
-        self.exit_option.triggered.connect(QApplication.quit)
-        self.open_user_guide_option = QAction('User Guide')
-        self.open_user_guide_option.triggered.connect(self.open_user_guide)
+        self.open_socket_window_action = QAction(text='Connect', parent=self)
+        self.open_socket_window_action.triggered.connect(self.handle_open_socket_window)
+        self.exit_action = QAction(text='Exit', parent=self)
+        self.exit_action.triggered.connect(self.handle_exit)
+        self.open_user_guide_action = QAction(text='User Guide', parent=self)
+        self.open_user_guide_action.triggered.connect(self.open_user_guide)
 
         # Create the menu bar and menu bar selections
         self.menu_bar = self.menuBar()
         self.file_menu = self.menu_bar.addMenu('File')
-        self.file_menu.addAction(self.exit_option)
+        self.file_menu.addAction(self.open_socket_window_action)
+        self.file_menu.addAction(self.exit_action)
         self.help_menu = self.menu_bar.addMenu('Help')
-        self.help_menu.addAction(self.open_user_guide_option)
+        self.help_menu.addAction(self.open_user_guide_action)
+
+        # Create the layout
+
+    def handle_open_socket_window(self) -> None:
+        """
+        Opens a window with `IP` and `PORT` QLineEdits and a `Connect` button.
+        Populate with IP and PORT global variables.
+        """
+        self.open_socket_window = OpenSocketWindow(self.sock)
+        self.open_socket_window.successful.connect(self.get_socket)
+        self.open_socket_window.show()
+
+    def get_socket(self, sock: SocketType) -> None:
+        self.sock = sock
+
+    def handle_exit(self) -> None:
+        if self.sock:
+            close_socket(self.sock)
+        QApplication.quit()
 
     def open_user_guide(self) -> None:
         print(get_root_dir())
