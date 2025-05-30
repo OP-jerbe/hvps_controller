07d120924dba2f79be7ceccd1f00a95c924bda3f
fizziksfool <erbe.joshua@gmail.com>
2025-05-29 20:17:14 -0700

    Adds doc string to run_app in main.py. Deletes other unnecessary comments.

--- main.py
+++ main.py
@@ -7,11 +7,18 @@ from src.gui.main_window import MainWindow
 
 
 def run_app() -> NoReturn:
+    """
+    Sets the version of application build,
+    creates the app and main window,
+    then executes the application event loop.
+    app.exec() == 0 when the event loop stops.
+    sys.exit(0) terminates the application.
+    """
     version = '1.0.0'
     app = QApplication([])
-    window = MainWindow(version=version)  # Create the main window from main_window.py
-    window.show()  # Show the window
-    sys.exit(app.exec())  # Start the application's event loop
+    window = MainWindow(version=version)
+    window.show()
+    sys.exit(app.exec())
 
 
 if __name__ == '__main__':
