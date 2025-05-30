2e6a07c454ec437f91ab2d41702718737aa322f7
OP-jerbe <j.erbe@oregon-physics.com>
2025-05-30 13:40:48 -0700

    Adds the HV enable and solenoid enable buttons to the main_window gui.

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -23,6 +23,7 @@ from qt_material import apply_stylesheet
 
 from helpers.helpers import close_socket, get_root_dir
 
+from ..hvps.hvps_api import HVPSv3
 from .open_socket_window import OpenSocketWindow
 
 
@@ -31,6 +32,9 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.version = version
         self.sock: Optional[SocketType] = sock
+        self.hvps: HVPSv3
+        if self.sock:
+            self.hvps = HVPSv3(self.sock)
         self.installEventFilter(self)
         self.create_gui()
 
@@ -38,7 +42,6 @@ class MainWindow(QMainWindow):
         window_width = 550
         window_height = 500
         self.setFixedSize(window_width, window_height)
-
         root_dir: Path = get_root_dir()
         icon_path: str = str(root_dir / 'assets' / 'hvps_icon.ico')
         self.setWindowIcon(QIcon(icon_path))
@@ -64,24 +67,80 @@ class MainWindow(QMainWindow):
         self.help_menu = self.menu_bar.addMenu('Help')
         self.help_menu.addAction(self.open_user_guide_action)
 
-        # Create the layout
+        # Create the widgets
+        self.hv_enable_btn = QPushButton('HV Disabled')
+        self.hv_enable_btn.setCheckable(True)
+        self.hv_enable_btn.clicked.connect(self.handle_hv_enable_btn)
+        self.sol_enable_btn = QPushButton('Solenoid Disabled')
+        self.sol_enable_btn.setCheckable(True)
+        self.sol_enable_btn.clicked.connect(self.handle_sol_enable_btn)
+
+        # Set the layout
+
+        btn_layout = QVBoxLayout()
+        btn_layout.addWidget(self.hv_enable_btn)
+        btn_layout.addWidget(self.sol_enable_btn)
+
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(btn_layout)
+
+        container = QWidget()
+        container.setLayout(main_layout)
+
+        self.setCentralWidget(container)
 
     def handle_open_socket_window(self) -> None:
         """
         Opens a window with `IP` and `PORT` QLineEdits and a `Connect` button.
         Populate with IP and PORT global variables.
+        If self.sock is not None, all QWidgets in OpenSocketWindow are disabled.
         """
         self.open_socket_window = OpenSocketWindow(self.sock)
         self.open_socket_window.successful.connect(self.get_socket)
         self.open_socket_window.show()
 
     def get_socket(self, sock: SocketType) -> None:
+        """
+        Gets the socket from the OpenSocketWindow Signal
+        """
         self.sock = sock
+        self.hvps = HVPSv3(self.sock)
 
     def handle_exit(self) -> None:
+        """
+        Handles what happens with the application is closed.
+        If there is socket connection, terminate the connection.
+        """
         if self.sock:
             close_socket(self.sock)
         QApplication.quit()
 
     def open_user_guide(self) -> None:
+        """
+        Open the user guide HTML
+        """
         print(get_root_dir())
+
+    def handle_hv_enable_btn(self) -> None:
+        """
+        Disables HV if the button has been checked.
+        Enables HV if the button has not been checked.
+        """
+        if not self.hv_enable_btn.isChecked():
+            # self.hvps.disable_high_voltage()
+            self.hv_enable_btn.setText('HV Disabled')
+        else:
+            # self.hvps.enable_high_voltage()
+            self.hv_enable_btn.setText('HV Enabled')
+
+    def handle_sol_enable_btn(self) -> None:
+        """
+        Disables solenoid current if the button has been checked.
+        Enables solenoid current if the button has been checked.
+        """
+        if not self.sol_enable_btn.isChecked():
+            # self.hvps.disable_solenoid_current()
+            self.sol_enable_btn.setText('Solenoid Disabled')
+        else:
+            # self.hvps.enable_solenoid_current()
+            self.sol_enable_btn.setText('Solenoid Enabled')
